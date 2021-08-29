1ef991f4f805cabf8329729b38e01f5d87c50bb6
Thien Tran <tranvanthien010799@gmail.com>
2021-08-29 21:43:12 +0700

    Add API detele and update role

--- src/main/java/com/dhtinh/demo/controller/RoleController.java
+++ src/main/java/com/dhtinh/demo/controller/RoleController.java
@@ -18,8 +18,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(ROLE)
 public class RoleController {
-    @Autowired
-    private RoleService roleService;
-    @Autowired
-    private ModelMapper mapper;
+	@Autowired
+	private RoleService roleService;
+	@Autowired
+	private ModelMapper mapper;
 
 	@CrossOrigin
-    @PostMapping
-	public ResponseEntity<RoleResponseModel> createRole(@RequestBody @Valid RoleRequestModel roleRequestModel, BindingResult bindingResult) {
+	@PostMapping
+	public ResponseEntity<RoleResponseModel> createRole(@RequestBody @Valid RoleRequestModel roleRequestModel,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
@@ -42,7 +46,7 @@ public class RoleController {
 		mapper.map(roleRequestModel, roleDTO);
 		RoleDTO roleCreated = roleService.createRole(roleDTO);
 
-		if(roleCreated == null){
+		if (roleCreated == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 
@@ -55,12 +59,38 @@ public class RoleController {
 	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<RoleResponseModel>> getRoles() {
-		List<RoleDTO> roleDTOs  = roleService.getRoles();
-	 	if(roleDTOs == null){
-	 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-	 	}
-	 	List<RoleResponseModel> roleResponseModels = mapper.map(roleDTOs, new TypeToken<List<RoleResponseModel>>(){}.getType());
-             return ResponseEntity.ok().body(roleResponseModels);
-	 }
-    
+		List<RoleDTO> roleDTOs = roleService.getRoles();
+		if (roleDTOs == null) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		List<RoleResponseModel> roleResponseModels = mapper.map(roleDTOs, new TypeToken<List<RoleResponseModel>>() {
+		}.getType());
+		return ResponseEntity.ok().body(roleResponseModels);
+	}
+
+	@CrossOrigin
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteRole(@PathVariable Long id) {
+		try {
+			roleService.deleteRole(id);
+			return ResponseEntity.ok(true);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
+	@CrossOrigin
+	@PutMapping("/{id}")
+	public ResponseEntity<RoleResponseModel> updateRole(@PathVariable Long id,	@RequestBody RoleRequestModel roleRequestModel) {
+		try {
+			RoleDTO roleDTO = new RoleDTO();
+			mapper.map(roleRequestModel, roleDTO);
+			RoleDTO roleUpdated = roleService.updateRole(id, roleDTO);
+			RoleResponseModel userResponseModel = new RoleResponseModel();
+			mapper.map(roleUpdated, userResponseModel);
+			return ResponseEntity.ok().body(userResponseModel);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
 }
