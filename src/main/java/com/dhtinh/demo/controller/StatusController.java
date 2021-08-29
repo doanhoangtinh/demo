271181d8a7c5051595019038360dce7c05f150bf
Thien Tran <tranvanthien010799@gmail.com>
2021-08-30 00:21:41 +0700

    Add API delete and update

--- src/main/java/com/dhtinh/demo/controller/StatusController.java
+++ src/main/java/com/dhtinh/demo/controller/StatusController.java
@@ -18,8 +18,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,14 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(STATUS)
 public class StatusController {
-    @Autowired
-    private StatusService statusService;
-    @Autowired
-    private ModelMapper mapper;
+	@Autowired
+	private StatusService statusService;
+	@Autowired
+	private ModelMapper mapper;
 
 	@CrossOrigin
-    @PostMapping
-	public ResponseEntity<StatusResponseModel> createStatus(@RequestBody @Valid StatusRequestModel statusRequestModel, BindingResult bindingResult) {
+	@PostMapping
+	public ResponseEntity<StatusResponseModel> createStatus(@RequestBody @Valid StatusRequestModel statusRequestModel,
+			BindingResult bindingResult) {
 		if (bindingResult.hasErrors()) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
@@ -42,7 +46,7 @@ public class StatusController {
 		mapper.map(statusRequestModel, statusDTO);
 		StatusDTO statusCreated = statusService.createStatus(statusDTO);
 
-		if(statusCreated == null){
+		if (statusCreated == null) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
 
@@ -55,12 +59,40 @@ public class StatusController {
 	@CrossOrigin
 	@GetMapping
 	public ResponseEntity<List<StatusResponseModel>> getStatuss() {
-		List<StatusDTO> statusDTOs  = statusService.getStatuses();
-	 	if(statusDTOs == null){
-	 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-	 	}
-	 	List<StatusResponseModel> statusResponseModels = mapper.map(statusDTOs, new TypeToken<List<StatusResponseModel>>(){}.getType());
-             return ResponseEntity.ok().body(statusResponseModels);
-	 }
-    
+		List<StatusDTO> statusDTOs = statusService.getStatuses();
+		if (statusDTOs == null) {
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}
+		List<StatusResponseModel> statusResponseModels = mapper.map(statusDTOs,
+				new TypeToken<List<StatusResponseModel>>() {
+				}.getType());
+		return ResponseEntity.ok().body(statusResponseModels);
+	}
+
+	@CrossOrigin
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Boolean> deleteStatus(@PathVariable Long id) {
+		try {
+			statusService.deleteStatus(id);
+			return ResponseEntity.ok(true);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
+
+	@CrossOrigin
+	@PutMapping("/{id}")
+	public ResponseEntity<StatusResponseModel> updateStatus(@PathVariable Long id,
+			@RequestBody StatusRequestModel statusRequestModel) {
+		try {
+			StatusDTO statusDTO = new StatusDTO();
+			mapper.map(statusRequestModel, statusDTO);
+			StatusDTO statusUpdate = statusService.updateStatus(id, statusDTO);
+			StatusResponseModel statusResponseModel = new StatusResponseModel();
+			mapper.map(statusUpdate, statusResponseModel);
+			return ResponseEntity.ok().body(statusResponseModel);
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+	}
 }
